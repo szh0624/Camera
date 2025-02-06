db5e083fe47d832d5dd989d04e4f37bb3712a0da
szh0624 <szh-18986026045@outlook.com>
2025-02-06 21:00:40 +0800

    study

--- kivy_study/窗口颜色.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.core.window import Window
-from kivy.utils import get_color_from_hex
-
-Window.clearcolor =get_color_from_hex("#FFFFFF")
-Apps=App()
-Apps.title="YFTHKKD App"
-Apps.run()
